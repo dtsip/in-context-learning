f8fa8d0d1390cc15380ec0a65804d3b6a92683ea
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2023-12-01 02:09:14 +0000

    removed unused line

--- src/consts.py
+++ src/consts.py
@@ -3,5 +3,3 @@ import torch
 
 SEQ_MODELS = ["gpt2", "lstm", "relu_attn"]
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-
-if torch.cuda.is_available() and model.name.split("_")[0] in SEQ_MODELS: device = "cuda"
